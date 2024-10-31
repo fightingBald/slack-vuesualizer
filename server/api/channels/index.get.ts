5916d686bc7da91dd3c983e15941cebaaf92f445
huayitang <tanghuayi906@outlook.com>
2024-10-31 15:16:11 +0100

    auth.ts

--- server/api/channels/index.get.ts
+++ server/api/channels/index.get.ts
@@ -1,14 +1,15 @@
-//server/api/channels/index.get.ts
 import { mongo } from '~/server/utils/mongo'
 import type { Channel } from '~~/types/Channel'
 
 export default defineEventHandler(async (event) => {
   const db = await mongo(event.context.mongouuid)
+  const userId = event.context.userId
+
   const channels = await db
-    .collection<Channel>('channels')
-    .find()
-    .sort({ name: 1 })
-    .toArray()
+      .collection<Channel>('channels')
+      .find({ members: userId }) // 添加 userId 过滤器
+      .sort({ name: 1 })
+      .toArray()
 
   return channels
 })
